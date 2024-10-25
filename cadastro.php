5f6e926ae2c1a24a4340d8b98735c32fd3da12c7
Júlia Rodrigues <heylavenderbee@gmail.com>
2024-10-25 10:41:16 -0300

    add files

---
+++ cadastro.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Cadastro</title>
+</head>
+<body>
+    <div class="form-container">
+        <form method="post">
+            Nome<br>
+            <input type="text" name="name">
+            <br><br>
+            Email<br>
+            <input type="email" name="email">
+            <br><br>
+            Senha<br>
+            <input type="password" name="password">
+            <br><br>
+            <input type="submit" name="submit">
+            <br>
+        </form>
+    </div>
+</body>
+</html>
